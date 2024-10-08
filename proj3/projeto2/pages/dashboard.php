0cfa84008cba4017c2087f2b6da507cc4978a72f
H-Saimon <hitalonsaimon@gmail.com>
2024-10-07 21:24:39 -0300

    Add files via upload

---
+++ proj3/projeto2/pages/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<?php include "head.php"; ?>
+
+<body class="">
+  <div class="wrapper ">
+    <?php include "menu.php"; ?>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <?php include "header.php"; ?>
+      <!-- End Navbar -->
+      <div class="content">
+        <?php include "cards.php"; ?>
+      </div>
+      <?php include "footer.php"; ?>
+    </div>
+  </div>
+  <?php include "js.php"; ?>
+</body>
+
+</html>
